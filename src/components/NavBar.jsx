87452e723478a41b9c3be37bb009d2b8a45c8f54
Emmanuel Cardenas <emmanuelctx17@gmail.com>
2025-03-05 11:10:03 -0600

    Updated About files and App fies

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="Navbar" style={{ background: 'lightblue', padding: '10px' }}>
+    <nav className="Navbar">
       <ul>
         <li><Link to="/">Home</Link></li> 
         <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/work">WorkExperience</Link></li>
+        <li><Link to="/work">Work Experience</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
